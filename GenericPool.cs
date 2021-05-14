946468be21fd6381dfe4014151535d4b2dd1a93a
Lucas McCully <thecarguru2401@gmail.com>
2021-05-14 00:58:21 -0300

    Delete GenericPool.cs

--- GenericPool.cs
+++
@@ -1,8 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class GenericPool : GenericObjectPool<ObjectTobePooled>
-{
-    public override void AddPoolReference(ObjectTobePooled objectToAddReference) => objectToAddReference.GetComponent<IObjectPool>().pool = this;
-}
